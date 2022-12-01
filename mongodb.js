e13a57a48a5b8ffeef7b6e4b74fc04e1b4edf79e
Ajay@2k22 <83329611+Ajay2k22@users.noreply.github.com>
2022-12-01 14:33:52 +0530

    Update mongodb.js
    mongodb connection suscessfully

--- mongodb.js
+++ mongodb.js
@@ -1,34 +1,19 @@
-const mongoose =require('mongoose');
+const mongoose = require('mongoose');
 
-const url=`mongodb://0.0.0.0:27017`
+const url = `mongodb://0.0.0.0:27017`
 
-const connection= async ()=>(
-    await mongoose.connect(url,(err)=>{
-        if (err){
+const connection = async () => (
+    await mongoose.connect(url, (err, res) => {
+        if (err) {
             console.log(`unable to connect ${err}`)
         }
+
         else{
             console.log(`Connected Sucessfully`)
         }
+
     })
-    
-    
 );
-const UserSchema = mongoose.Schema({
-    name: String,
-    age: Number,
-    salary: Number
-})
 
-const mymodel = mongoose.model('Employees', UserSchema)
-
-const emp=new mymodel({
-    "name":"max",
-    "age":22,
-    "salary":54309
-})
-console.log(emp.name)
-console.log(emp)
-
-
+connection()
 
