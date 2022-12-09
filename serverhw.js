989719c19f5355589de1e4ada1219c4b90744bfd
Ajay@2k22 <83329611+Ajay2k22@users.noreply.github.com>
2022-12-09 18:35:30 +0530

    Add files via upload

---
+++ serverhw.js
@@ -0,0 +1,27 @@
+const MongoClient = require("mongodb").MongoClient;
+const url = "mongodb://127.0.0.1:27017/";
+
+const connect = MongoClient.connect(url, function (err, client) {
+    if (err) throw err
+    console.log("Connected Sucessfully")
+    const db = client.db("Employee")
+    var newkey = db.collection('emp').findOne({}, async (err, res) => {
+        if (err) throw err
+        console.log('fetched data sucessfully')
+        console.log(res);
+        response.firstname = await res.name;
+        response.age = await res.age;
+        response.salary = await res.Salary;
+        console.log(response)
+        client.close()
+    });
+});
+
+connect
+
+var response = {
+    firstname: "",
+    age: "",
+    salary: ""
+}
+module.exports = response;
